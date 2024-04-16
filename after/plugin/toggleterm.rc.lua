3186d1d0f4aad91d612a6c19ac10d9519d2632a1
y-mattsuda <m.youichi1225@icloud.com>
2024-04-17 00:23:50 +0900

    copy LazyVim starter repo

--- after/plugin/toggleterm.rc.lua
+++
@@ -1,8 +0,0 @@
-require("toggleterm").setup({
-	size = 10,
-	open_mapping = [[<C-t>]],
-	direction = "horizontal",
-	float_opts = {
-		border = "curved",
-	},
-})
--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
-require("base")
-require("plugins")
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = { "plugins.lua" },
-	command = "PackerCompile",
-})
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
